33d03d34525c7a2ec594f0d489551da392d07309
BaoTengfei <917475409@qq.com>
2017-08-13 23:25:46 +0800

    task15

---
+++ task15/css/header.js
@@ -0,0 +1,19 @@
+
+/* window.click=function(){
+     var allBtn=document.getElementById('btn');
+     var allHomepage=document.getElementsByClassName('homepage');
+     allBtn.onclick=function () {
+         if(allHomepage.style.display=='none'){
+             allHomepage.style.display='block';
+         }
+         else{
+             allHomepage.style.display='none';
+         }
+     }
+ }*/
+window.onload=function () {
+        btn.onclick=function () {
+            a.style.display=
+                !a.style.display||a.style.display=='block'?'none':'block';
+        }
+    }
